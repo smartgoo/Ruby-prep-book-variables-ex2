3bc989a2db510736040de0ee28f66da83b3ed36a
Chuck Vallone <cvallone28@gmail.com>
2016-10-02 17:34:01 -0400

    ruby prep book variables ex2

---
+++ age.rb
@@ -0,0 +1,15 @@
+q_part_1 = 'In '
+q_part_2 = ' years you will be:'
+
+puts 'How old are you?'
+
+age = gets.chomp.to_i
+
+puts q_part_1 + '10'.to_s + q_part_2
+puts (age + 10).to_s
+puts q_part_1 + '20'.to_s + q_part_2
+puts (age + 20).to_s
+puts q_part_1 + '30'.to_s + q_part_2
+puts (age + 30).to_s
+puts q_part_1 + '40'.to_s + q_part_2
+puts (age + 40).to_s
